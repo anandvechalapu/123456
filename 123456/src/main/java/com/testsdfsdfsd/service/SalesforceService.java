2b3840675ae7272cb3185b20ae33611fee7f3f1e
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-19 16:21:06 +0530

    Updated

--- 123456/src/main/java/com/testsdfsdfsd/service/SalesforceService.java
+++ 123456/src/main/java/com/testsdfsdfsd/service/SalesforceService.java
@@ -1,34 +1,55 @@
 package com.testsdfsdfsd.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import com.testsdfsdfsd.repository.SalesforceRepository;
-
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.testsdfsdfsd.model.Opportunity;
+import com.testsdfsdfsd.repository.SalesforceRepository;
 
 @Service
-@Transactional
 public class SalesforceService {
 
-    private SalesforceRepository salesforceRepository;
-
-    public SalesforceService(SalesforceRepository salesforceRepository) {
-        this.salesforceRepository = salesforceRepository;
-    }
-
-    public List<Opportunity> findOpportunitiesByAccountId(Long accountId) {
-        return salesforceRepository.findOpportunitiesByAccountId(accountId);
-    }
-
-    public List<Opportunity> findOpportunitiesCreatedMoreThan30DaysAgo(LocalDateTime thirtyDaysAgo) {
-        return salesforceRepository.findOpportunitiesCreatedMoreThan30DaysAgo(thirtyDaysAgo);
-    }
-
-    public int updateOpportunityStageToCloseLost(LocalDateTime thirtyDaysAgo) {
-        return salesforceRepository.updateOpportunityStageToCloseLost(thirtyDaysAgo);
-    }
-
+	@Autowired
+	private SalesforceRepository salesforceRepository;
+	
+	public List<Opportunity> findAllByStatus(String status) {
+		return salesforceRepository.findAllByStatus(status);
+	}
+	
+	public List<Opportunity> findAllByNameOrOwnerNameOrStageOrAmountOrClosingDate(String name, String ownerName, String stage, Double amount, Date closingDate) {
+		return salesforceRepository.findAllByNameOrOwnerNameOrStageOrAmountOrClosingDate(name, ownerName, stage, amount, closingDate);
+	}
+	
+	public List<Opportunity> findAllByOrderByAmountAsc() {
+		return salesforceRepository.findAllByOrderByAmountAsc();
+	}
+	
+	public List<Opportunity> findAllByOrderByAmountDesc() {
+		return salesforceRepository.findAllByOrderByAmountDesc();
+	}
+	
+	public List<Opportunity> findAllByOrderByStageAsc() {
+		return salesforceRepository.findAllByOrderByStageAsc();
+	}
+	
+	public List<Opportunity> findAllByOrderByStageDesc() {
+		return salesforceRepository.findAllByOrderByStageDesc();
+	}
+	
+	public List<Opportunity> findAllByOrderByClosingDateAsc() {
+		return salesforceRepository.findAllByOrderByClosingDateAsc();
+	}
+	
+	public List<Opportunity> findAllByOrderByClosingDateDesc() {
+		return salesforceRepository.findAllByOrderByClosingDateDesc();
+	}
+	
+	public Page<Opportunity> findAll(Pageable pageable) {
+		return salesforceRepository.findAll(pageable);
+	}
 }
