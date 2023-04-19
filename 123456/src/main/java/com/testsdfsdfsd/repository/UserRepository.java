8bc987b10c99905d6053a23496422ea9c34272ca
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-19 16:18:25 +0530

    Updated

--- 123456/src/main/java/com/testsdfsdfsd/repository/UserRepository.java
+++ 123456/src/main/java/com/testsdfsdfsd/repository/UserRepository.java
@@ -1,14 +1,14 @@
 package com.testsdfsdfsd.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import com.testsdfsdfsd.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.testsdfsdfsd.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
-
     User findByEmail(String email);
-
     void deleteByUsername(String username);
-
+    void deleteByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
